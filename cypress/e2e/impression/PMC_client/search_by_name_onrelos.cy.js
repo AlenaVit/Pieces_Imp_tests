2fb1d306017c6fc23a51ba9b517aceacc4967dca
Lena <evitko@dev-team.com>
2022-12-25 15:39:17 +0100

    URL changed

--- cypress/e2e/impression/PMC_client/search_by_name_onrelos.cy.js
+++ cypress/e2e/impression/PMC_client/search_by_name_onrelos.cy.js
@@ -1,7 +1,7 @@
 describe('login to the Impression', () => {
   beforeEach(() => {
   cy.setPMCCookie()
-  cy.visit('https://staging.myimpression.piecestech.com')
+  cy.visit('https://staging-impression.piecestech.com')
   cy.viewport(1920, 1080)
   })
 
